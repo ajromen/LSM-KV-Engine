8f3acbb7475c2e7ed2cdc6f40ef22bf086b6d407
Aleksandar Pavlović <109547906+ajromen@users.noreply.github.com>
2025-12-11 13:42:33 +0100

    added todo comments

--- cmd/lsm-kv-engine/main.go
+++ cmd/lsm-kv-engine/main.go
@@ -10,7 +10,7 @@ import (
 func main() {
 	flags := ParseFlags()
 	fmt.Println("Config file:", flags.ConfigPath)
-
+	// TODO napraiviti engine.go i poslati mu config i flagove
 	RunCli()
 }
 
@@ -29,6 +29,8 @@ func RunCli() {
 		parts := strings.Fields(line)
 
 		switch parts[0] {
+		case "put":
+			//TODO pozivati metode engine-a za put get delete ( za sad )
 		case "exit", "quit":
 			return
 		case "help":
