a956379ea90f529701a6aa472ea85cbfee5eaa95
Nemanja Rikic <nemanjarikic01@gmail.com>
2025-12-15 21:21:12 +0100

    Updated memtable

--- internal/memtable/factory.go
+++ internal/memtable/factory.go
@@ -3,13 +3,13 @@ package memtable
 func NewMemtable(memType string, maxSize int) Memtable {
 	if memType == "hashmap" {
 		return NewHashMap(maxSize)
+	} else if memType == "skiplist" {
+		return NewSkipListMem(maxSize)
 	} else {
 		return nil
 	}
 	/*
-		else if memType == "skiplist" {
-			return NewSkipList(maxSize)
-		}
+
 		else if memType == "btree" {
 			return newBTree(maxSize)
 		}
