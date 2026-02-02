9327c88b50214f54ed6cee99aae923919c657688
Nemanja Rikic <171062690+Rikiccc@users.noreply.github.com>
2026-02-02 15:57:04 +0100

    restarted work on SSTable

--- internal/utils/io_operations.go
+++ internal/utils/io_operations.go
@@ -2,9 +2,16 @@ package utils
 
 import (
 	"encoding/binary"
+	"errors"
 	"os"
 )
 
+type Uint128 struct {
+	high uint64
+	low  uint64
+}
+
+// use this for writing uint32 || uint64
 func WriteUvarint(file *os.File, value uint64) error {
 	buf := make([]byte, binary.MaxVarintLen64)
 	n := binary.PutUvarint(buf, value)
@@ -15,6 +22,7 @@ func WriteUvarint(file *os.File, value uint64) error {
 	return nil
 }
 
+// use this for reading uint32 || uint64
 func ReadUvarint(file *os.File) (uint64, error) {
 	var result uint64
 	var shift uint
@@ -31,4 +39,54 @@ func ReadUvarint(file *os.File) (uint64, error) {
 		shift += 7
 	}
 	return result, nil
+}
+
+// use this for writing uint128 (timestamp 16B)
+func WriteUvarint128(file *os.File, value Uint128) error {
+	high := value.high
+	low := value.low
+	for i := 0; i < 20; i++ {
+		b := byte(low & 0x7F)
+		low >>= 7
+		if high != 0 {
+			b |= byte(high<<((64-7*i)%64)) & 0x7F
+		}
+		if high != 0 || low != 0 {
+			b |= 0x80
+		}
+		_, err := file.Write([]byte{b})
+		if err != nil {
+			return err
+		}
+		if high == 0 && low == 0 {
+			break
+		}
+	}
+	return nil
+}
+
+// use this for reading uint128 (timestamp 16B)
+func ReadUvarint128(file *os.File) (Uint128, error) {
+	var value Uint128
+	var shift uint
+	for i := 0; i < 20; i++ {
+		b := make([]byte, 1)
+		n, err := file.Read(b)
+		if err != nil {
+			return value, err
+		}
+		if n != 1 {
+			return value, errors.New("error reading uint128")
+		}
+		if shift < 64 {
+			value.low |= uint64(b[0]&0x7F) << shift
+		} else {
+			value.high |= uint64(b[0]&0x7F) << (shift - 64)
+		}
+		if b[0]&0x80 == 0 {
+			break
+		}
+		shift += 7
+	}
+	return value, nil
 }
