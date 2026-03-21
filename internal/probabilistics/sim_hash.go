c9ded22e2f2190ab9402b6f84cad1b1afc808014
L0SM1V <milosvukic1990@gmail.com>
2026-03-21 20:51:15 +0100

    Update sim_hash.go

--- internal/probabilistics/sim_hash.go
+++ internal/probabilistics/sim_hash.go
@@ -1,5 +1,7 @@
 package probabilistic
 
+import "github.com/ajromen/LSM-KV-Engine/internal/config"
+
 const (
 	// Binarni format:
 	// 4B magic + u64(seedLen) + seed + u8(hasFingerprint) + u64(fingerprint)
@@ -13,4 +15,8 @@ type SimHash struct {
 	hashFn         HashWithSeed
 	fingerprint    uint64
 	hasFingerprint bool
+}
+
+func NewSimHash(cfg config.SimHashConfig) *SimHash {
+	return NewSimHashWithParams(cfg, nil)
 }
