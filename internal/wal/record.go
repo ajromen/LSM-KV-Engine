d31f9b19c477e1e21f83ccfc9c578bd65c9d2f1a
Kristian Zhou-Gubić <kristianzhougubic1@gmail.com>
2026-03-24 00:39:36 +0100

    add new record layout

--- internal/wal/record.go
+++ internal/wal/record.go
@@ -7,31 +7,59 @@ import (
 )
 
 /*
-   +---------------+-----------------+---------------+---------------+-----------------+-...-+--...--+
-   |    CRC (4B)   | Timestamp (8B) | Tombstone(1B) | Key Size (8B) | Value Size (8B) | Key | Value |
-   +---------------+-----------------+---------------+---------------+-----------------+-...-+--...--+
-   CRC = 32bit hash computed over the payload using CRC
-   Key Size = Length of the Key data
-   Tombstone = If this record was deleted and has a value
-   Value Size = Length of the Value data
-   Key = Key data
-   Value = Value data
-   Timestamp = Timestamp of the operation in seconds
+	+---------------+-----------------+--------------+---------------+--------------+---------------+---------------+-----------------+-...-+--...--+
+	|    CRC (4B)   | Timestamp (8B)  | FragType(1B) | Tombstone(1B) | RecType(1B)  |   TxnID (8B)  | Key Size (8B) | Value Size (8B) | Key | Value |
+	+---------------+-----------------+--------------+---------------+--------------+---------------+---------------+-----------------+-...-+--...--+
+
+	CRC = 32bit hash computed over the rest of the record using CRC
+	Timestamp = Timestamp of the record
+	FragType = Fragment type of the record (FULL, FIRST, MIDDLE, LAST)
+	Tombstone = If this record is a delete operation
+	RecType = Record Type (SINGLE, START, TRANSACTION, COMMIT)
+	TxnID = Transaction ID
+	Key Size = Length of the Key data
+   	Value Size = Length of the Value data
+	Key = Key data
+	Value = Value data
 */
 
 const (
 	CRC_SIZE        = 4
 	TIMESTAMP_SIZE  = 8
+	FRAGTYPE_SIZE   = 1
 	TOMBSTONE_SIZE  = 1
+	RECTYPE_SIZE    = 1
+	TXNID_SIZE      = 8
 	KEY_SIZE_SIZE   = 8
 	VALUE_SIZE_SIZE = 8
 
 	CRC_START        = 0
 	TIMESTAMP_START  = CRC_START + CRC_SIZE
-	TOMBSTONE_START  = TIMESTAMP_START + TIMESTAMP_SIZE
-	KEY_SIZE_START   = TOMBSTONE_START + TOMBSTONE_SIZE
+	FRAGTYPE_START   = TIMESTAMP_START + TIMESTAMP_SIZE
+	TOMBSTONE_START  = FRAGTYPE_START + FRAGTYPE_SIZE
+	RECTYPE_START    = TOMBSTONE_START + TOMBSTONE_SIZE
+	TXNID_START      = RECTYPE_START + RECTYPE_SIZE
+	KEY_SIZE_START   = TXNID_START + TXNID_SIZE
 	VALUE_SIZE_START = KEY_SIZE_START + KEY_SIZE_SIZE
 	KEY_START        = VALUE_SIZE_START + VALUE_SIZE_SIZE
+)
+
+type FragmentType uint8
+
+const (
+	FULL = iota
+	FIRST
+	MIDDLE
+	LAST
+)
+
+type RecordType uint8
+
+const (
+	SINGLE = iota
+	START
+	TRANSACTION
+	COMMIT
 )
 
 type Record struct {
@@ -39,6 +67,16 @@ type Record struct {
 	Tombstone bool
 	Key       []byte
 	Value     []byte
+}
+
+type WALRecord struct {
+	CRC       uint32
+	FragType  FragmentType
+	RecType   RecordType
+	TxnID     uint64
+	KeySize   uint64
+	ValueSize uint64
+	Record    Record
 }
 
 func Encode(r Record) []byte {
