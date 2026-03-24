36f91e3b0570c6504c2fa84df9fd5dd8b6643090
Kristian Zhou-Gubić <kristianzhougubic1@gmail.com>
2026-03-25 00:11:40 +0100

    add block helper methods

--- internal/wal/block.go
+++ internal/wal/block.go
@@ -1,5 +1,7 @@
 package wal
 
+import "errors"
+
 type Block struct {
 	Data     []byte
 	Size     int
@@ -12,4 +14,40 @@ func NewBlock(size int) *Block {
 		Size:     size,
 		WritePos: 0,
 	}
+}
+
+func (b *Block) Remaining() int {
+	return b.Size - b.WritePos
+}
+
+func (b *Block) Used() int {
+	return b.WritePos
+}
+
+func (b *Block) IsFull() bool {
+	return b.WritePos >= b.Size
+}
+
+func (b *Block) CanFit(n int) bool {
+	return n <= b.Remaining()
+}
+
+func (b *Block) Write(data []byte) (int, error) {
+	if len(data) == 0 {
+		return 0, nil
+	}
+	if b.IsFull() {
+		return 0, errors.New("block is full")
+	}
+
+	n := copy(b.Data[b.WritePos:], data)
+	b.WritePos += n
+	return n, nil
+}
+
+func (b *Block) Reset() {
+	for i := range b.Data {
+		b.Data[i] = 0
+	}
+	b.WritePos = 0
 }
