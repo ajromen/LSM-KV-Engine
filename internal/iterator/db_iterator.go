fe36b7540e9e14259baa84470fcd64daa027e907
L0SM1V <milosvukic1990@gmail.com>
2026-03-30 20:20:48 +0200

    Update db_iterator.go

--- internal/iterator/db_iterator.go
+++ internal/iterator/db_iterator.go
@@ -1,5 +1,7 @@
 package iterator
 
+import "bytes"
+
 type Entry struct {
 	Key           []byte
 	Value         []byte
@@ -20,3 +22,166 @@ const (
 	dbWinnerMem
 	dbWinnerSST
 )
+
+func NewDBIterator(memIterator Iterator[Entry], tableIterator Iterator[Entry]) *DBIterator {
+	it := &DBIterator{
+		memIterator:   memIterator,
+		tableIterator: tableIterator,
+	}
+	it.SeekToFirst()
+	return it
+}
+
+func (it *DBIterator) Valid() bool {
+	return it != nil && it.valid
+}
+
+func (it *DBIterator) SeekToFirst() {
+	if it.memIterator != nil {
+		it.memIterator.SeekToFirst()
+	}
+	if it.tableIterator != nil {
+		it.tableIterator.SeekToFirst()
+	}
+	it.advance()
+}
+
+func (it *DBIterator) SeekToLast() {
+	panic("DBIterator: SeekToLast not implemented")
+}
+
+func (it *DBIterator) Seek(key Entry) {
+	if it.memIterator != nil {
+		it.memIterator.Seek(key)
+	}
+	if it.tableIterator != nil {
+		it.tableIterator.Seek(key)
+	}
+	it.advance()
+}
+
+func (it *DBIterator) advance() {
+	for {
+		switch it.selectWinner() {
+		case dbWinnerNone:
+			it.current = nil
+			it.valid = false
+			return
+
+		case dbWinnerMem:
+			entry := it.memIterator.Key()
+			keyCopy := append([]byte(nil), entry.Key...)
+			valCopy := append([]byte(nil), entry.Value...)
+
+			it.skipKey(keyCopy)
+
+			if entry.Tombstone {
+				continue
+			}
+
+			it.current = &Entry{
+				Key:           keyCopy,
+				Value:         valCopy,
+				Tombstone:     false,
+				TimestampHigh: entry.TimestampHigh,
+				TimestampLow:  entry.TimestampLow,
+			}
+			it.valid = true
+			return
+
+		case dbWinnerSST:
+			entry := it.tableIterator.Key()
+			keyCopy := append([]byte(nil), entry.Key...)
+			valCopy := append([]byte(nil), entry.Value...)
+
+			it.skipKey(keyCopy)
+
+			if entry.Tombstone {
+				continue
+			}
+
+			it.current = &Entry{
+				Key:           keyCopy,
+				Value:         valCopy,
+				Tombstone:     false,
+				TimestampHigh: entry.TimestampHigh,
+				TimestampLow:  entry.TimestampLow,
+			}
+			it.valid = true
+			return
+		}
+	}
+}
+
+func (it *DBIterator) skipKey(key []byte) {
+	for it.memIterator != nil && it.memIterator.Valid() && bytes.Equal(it.memIterator.Key().Key, key) {
+		it.memIterator.Next()
+	}
+	for it.tableIterator != nil && it.tableIterator.Valid() && bytes.Equal(it.tableIterator.Key().Key, key) {
+		it.tableIterator.Next()
+	}
+}
+
+func (it *DBIterator) selectWinner() int {
+	memValid := it.memIterator != nil && it.memIterator.Valid()
+	sstValid := it.tableIterator != nil && it.tableIterator.Valid()
+
+	if !memValid && !sstValid {
+		return dbWinnerNone
+	}
+	if memValid && !sstValid {
+		return dbWinnerMem
+	}
+	if !memValid && sstValid {
+		return dbWinnerSST
+	}
+
+	memEntry := it.memIterator.Key()
+	sstEntry := it.tableIterator.Key()
+
+	if compareEntries(memEntry, sstEntry) <= 0 {
+		return dbWinnerMem
+	}
+	return dbWinnerSST
+}
+
+func compareEntries(a, b Entry) int {
+	if c := bytes.Compare(a.Key, b.Key); c != 0 {
+		return c
+	}
+
+	switch compareUint128(a.TimestampHigh, a.TimestampLow, b.TimestampHigh, b.TimestampLow) {
+	case 1:
+		return -1
+	case -1:
+		return 1
+	default:
+		return 0
+	}
+}
+
+func compareUint128(aHigh, aLow, bHigh, bLow uint64) int {
+	if aHigh > bHigh {
+		return 1
+	}
+	if aHigh < bHigh {
+		return -1
+	}
+	if aLow > bLow {
+		return 1
+	}
+	if aLow < bLow {
+		return -1
+	}
+	return 0
+}
+
+func copyEntry(e Entry) Entry {
+	return Entry{
+		Key:           append([]byte(nil), e.Key...),
+		Value:         append([]byte(nil), e.Value...),
+		Tombstone:     e.Tombstone,
+		TimestampHigh: e.TimestampHigh,
+		TimestampLow:  e.TimestampLow,
+	}
+}
