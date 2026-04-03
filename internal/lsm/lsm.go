25053c3ebff808d95587a1df71624be11a1f4a28
L0SM1V <milosvukic1990@gmail.com>
2026-04-03 11:16:46 +0200

    Update lsm.go

--- internal/lsm/lsm.go
+++ internal/lsm/lsm.go
@@ -42,6 +42,7 @@ func NewLSM(dataDir string) (*LSM, error) {
 }
 
 func (l *LSM) onFlush(entries []memtable.MemtableEntry) {
+	fmt.Printf("Flushing %d entries\n", len(entries))
 	err := l.sstableManager.FlushToSSTable(entries)
 	if err != nil {
 		panic(fmt.Errorf("error flushing memtable entries: %v", err))
