c92b88989ed1760599b3f888d72ceceb3c6e6855
Kristian Zhou-Gubić <kristianzhougubic1@gmail.com>
2026-04-04 03:08:09 +0200

    add append and rotate

--- internal/wal/wal.go
+++ internal/wal/wal.go
@@ -1,7 +1,9 @@
 package wal
 
 import (
+	"errors"
 	"fmt"
+	"os"
 	"path/filepath"
 
 	"github.com/ajromen/LSM-KV-Engine/internal/block"
@@ -16,7 +18,7 @@ type WAL struct {
 	BM            *block.BlockManager
 }
 
-func OpenWAL(dir string, blockSize int, maxBlocks int) (*WAL, error) {
+func OpenWAL(dir string, blockSize int, maxBlocks int) (*WAL, error) { //always opens from scratch, next segment id always 2, no matter how many files are already here
 	if dir == "" {
 		return nil, fmt.Errorf("wal dir is empty")
 	}
@@ -50,6 +52,88 @@ func OpenWAL(dir string, blockSize int, maxBlocks int) (*WAL, error) {
 
 	return w, nil
 
+}
+
+func (w *WAL) Append(r Record) error {
+	if w == nil {
+		return fmt.Errorf("wal is nil")
+	}
+	if w.ActiveSegment == nil {
+		return fmt.Errorf("active segment is nil")
+	}
+	err := w.ActiveSegment.Append(r)
+	if err == nil {
+		return nil
+	}
+	if !w.ActiveSegment.IsFull() {
+		return err
+	}
+
+	err = w.RotateSegment()
+	if err != nil {
+		return err
+	}
+
+	return w.ActiveSegment.Append(r)
+}
+
+func (w *WAL) RotateSegment() error {
+	if w == nil {
+		return fmt.Errorf("wal is nil")
+	}
+	if w.ActiveSegment == nil {
+		return fmt.Errorf("active segment is nil")
+	}
+	err := w.ActiveSegment.Sync()
+	if err != nil {
+		return err
+	}
+
+	newPath := w.SegmentPath(w.NextSegmentID)
+	seg, err := OpenSegment(w.NextSegmentID, newPath, w.MaxBlocks, w.BM)
+	if err != nil {
+		return err
+	}
+	w.ActiveSegment = seg
+	w.NextSegmentID++
+	return nil
+}
+
+func (w *WAL) Sync() error {
+	if w == nil {
+		return fmt.Errorf("wal is nil")
+	}
+	if w.ActiveSegment == nil {
+		return fmt.Errorf("active segment is nil")
+	}
+	return w.ActiveSegment.Sync()
+}
+
+func (w *WAL) PrintAll() error { //func for debugging
+	id := uint(1)
+	for {
+		path := w.SegmentPath(uint64(id))
+		if _, err := os.Stat(path); errors.Is(err, os.ErrNotExist) {
+			break
+		}
+		s, err := OpenSegment(uint64(id), w.SegmentPath(uint64(id)), w.MaxBlocks, w.BM)
+		if err != nil {
+			break
+		}
+		bindex := 0
+		fmt.Println("Segment", id)
+		for {
+			block, err := s.ReadBlock(uint32(bindex))
+			if err != nil {
+				break
+			}
+			fmt.Println(block)
+			bindex++
+		}
+		id++
+
+	}
+	return nil
 }
 
 func (w *WAL) SegmentPath(id uint64) string {
